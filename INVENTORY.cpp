6bac14e10181745507b4d9c63d4669b840a185b1
desreq <117791629+desreq@users.noreply.github.com>
2022-11-17 21:38:02 -0600

    Update INVENTORY.cpp

--- INVENTORY.cpp
+++ INVENTORY.cpp
@@ -7,8 +7,9 @@
 using namespace std;
     
 void Inventory::readfile(vector<Inventory> &list){
-	ifstream infile;
+    ifstream infile;
     string line;
+    Inventory tmp;
 
     infile.open("inventory.txt");
 
@@ -29,27 +30,32 @@ void Inventory::readfile(vector<Inventory> &list){
 
         if(line != "")
         {
-            string name,directorName,genre,price,stock,movieID;
+            string name;
+            string directorName;
+            string genre;
+            float price;
+            int stock;
+            int movieID;
 
             name = line;
-
+            cin.ignore();
             getline(infile, directorName);
 			getline(infile, genre);
-			getline(infile, price);
-			getline(infile,stock);
-			getline(infile,movieID);
-            Inventory temp(name,directorName,genre,price,stock,movieID);
+			cin >> price;
+			cin >> stock;
+			cin >> movieID;
+            Inventory tmp(name,directorName,genre,price,stock,movieID);
 
-            playlist.push_back(temp);
+            list.push_back(tmp);
         }
     }
 
     return;
 }
-}
 
 
-void Inventory::viewAllInventory(){
+// display functions
+void Inventory::viewAllInventory(vector<Inventory> &list){
         for(int i=0; i< list.size(); ++i){
             Inventory currItem;
             currItem = list.at(i);
@@ -58,15 +64,15 @@ void Inventory::viewAllInventory(){
         }
 		return;
 }
-void Inventory::viewByDirectory(){
+void Inventory::viewByDirector(vector<Inventory> &list){
 	    for(int i=0; i <list.size(); i = i+1 ){
         int indexSmallest= i;
         for(int j = i+1; j<list.size(); j = j+1) {
             int compare;
             string director1;
             string director2;
-            title1 = list[j].getDirectorName();
-            title2 = list[indexSmallest].getDirectorName();
+            director1 = list[j].getDirectorName();
+            director2 = list[indexSmallest].getDirectorName();
             // compare the director at the smallest index and the index before the smallest
             compare = director2.compare(director1);
             // swap the director names if the index before the smallest is actually the smallest
@@ -74,7 +80,7 @@ void Inventory::viewByDirectory(){
                 indexSmallest = j;
             }
         }
-        swap(playlist[i], playlist[indexSmallest]);
+        swap(list[i], list[indexSmallest]);
 }
 
 	 for(int i=0; i< list.size(); ++i){
@@ -85,20 +91,20 @@ void Inventory::viewByDirectory(){
         }
 return;
 }
-void Inventor:: viewLowtoHigh(){
-	    for(int i=0; i <playlist.size(); i = i+1 ){
+void Inventory:: viewLowtoHigh(vector<Inventory> &list){
+	    for(int i=0; i <list.size(); i = i+1 ){
         int indexSmallest= i;
-        for(int j = i+1; j<playlist.size(); j = j+1) {
+        for(int j = i+1; j<list.size(); j = j+1) {
             int compare;
-            string title1;
-            string title2;
-            price1 = playlist[j].getPrice();
-            price2 = playlist[indexSmallest].getPrice();
+            string price1;
+            string price2;
+            price1 = list[j].getPrice();
+            price2 = list[indexSmallest].getPrice();
            if (price1 >= price2) {
                 indexSmallest = j;
             }
         }
-        swap(playlist[i], playlist[indexSmallest]);
+        swap(list[i], list[indexSmallest]);
 
     }
 	for(int i=0; i< list.size(); ++i){
@@ -111,32 +117,159 @@ void Inventor:: viewLowtoHigh(){
 
 
 }
-void Inventor:: viewHightoLow(){
-		    for(int i=0; i <playlist.size(); i = i+1 ){
-        int indexSmallest= i;
-        for(int j = i+1; j<playlist.size(); j = j+1) {
+void Inventory:: ViewHightoLow(vector<Inventory> &list) {
+    for (int i = 0; i < list.size(); i = i + 1) {
+        int indexSmallest = i;
+        for (int j = i + 1; j < list.size(); j = j + 1) {
             int compare;
-            string title1;
-            string title2;
-            price1 = playlist[j].getPrice();
-            price2 = playlist[indexSmallest].getPrice();
-           if (price2 >= price1) {
+            string price1;
+            string price2;
+            price1 = list[j].getPrice();
+            price2 = list[indexSmallest].getPrice();
+            if (price2 >= price1) {
                 indexSmallest = j;
             }
         }
-        swap(playlist[i], playlist[indexSmallest]);
+        swap(list[i], list[indexSmallest]);
 
     }
-	for(int i=0; i< list.size(); ++i){
-            Inventory currItem;
-            currItem = list.at(i);
-            cout << currItem.getName() << " - " << currItem.getPrice() << endl;
-			
+    for (int i = 0; i < list.size(); ++i) {
+        Inventory currItem;
+        currItem = list.at(i);
+        cout << currItem.getName() << " - " << currItem.getPrice() << endl;
+
+    }}
+
+
+	
+
+//searches
+bool Inventory::searchByDirector(vector<Inventory>&list, string directorName) {
+    for(int i=0; i <list.size(); i = i+1 ){
+        int indexSmallest= i;
+        for(int j = i+1; j<list.size(); j = j+1) {
+            int compare;
+            string director1;
+            string director2;
+            director1 = list[j].getDirectorName();
+            director2 = list[indexSmallest].getDirectorName();
+            // compare the director at the smallest index and the index before the smallest
+            compare = director2.compare(director1);
+            // swap the director names if the index before the smallest is actually the smallest
+            if (compare >= 1) {
+                indexSmallest = j;
+            }
         }
-		return;
-	
+        swap(list[i], list[indexSmallest]);
+    }
+
+    int low = 0;
+    int high = list.size() - 1;
+    int mid;
+
+    while (high >= low) {
+        mid = (low + high) / 2;
+        // if the middle song title is lower than then the song title that is being searched for change the low and mid
+        if (list[mid].getDirectorName().compare(directorName) < 0) {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        }
+            // if the middle song title is higher than then the song title that is being searched for change the high and mid
+        else if(list[mid].getDirectorName().compare(directorName) > 0){
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+        else{
+            return mid;
+        }
+        return -1;
+    }
 }
-	
+
+bool Inventory::searchByGenre(vector<Inventory> &list, string genre) {
+    for(int i=0; i <list.size(); i = i+1 ){
+        int indexSmallest= i;
+        for(int j = i+1; j<list.size(); j = j+1) {
+            int compare;
+            string genre1;
+            string genre2;
+            genre1 = list[j].getGenre();
+            genre2 = list[indexSmallest].getGenre();
+            // compare the director at the smallest index and the index before the smallest
+            compare = genre2.compare(genre1);
+            // swap the director names if the index before the smallest is actually the smallest
+            if (compare >= 1) {
+                indexSmallest = j;
+            }
+        }
+        swap(list[i], list[indexSmallest]);
+    }
+
+    int low = 0;
+    int high = list.size() - 1;
+    int mid;
+
+    while (high >= low) {
+        mid = (low + high) / 2;
+        // if the middle song title is lower than then the song title that is being searched for change the low and mid
+        if (list[mid].getGenre().compare(genre) < 0) {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        }
+            // if the middle song title is higher than then the song title that is being searched for change the high and mid
+        else if(list[mid].getGenre().compare(genre) > 0){
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+        else{
+            return mid;
+        }
+        return -1;
+    }
+
+}
+
+bool Inventory::searchByName(vector<Inventory> &list, string Name) {
+    for(int i=0; i <list.size(); i = i+1 ){
+        int indexSmallest= i;
+        for(int j = i+1; j<list.size(); j = j+1) {
+            int compare;
+            string Name1;
+            string Name2;
+            Name1 = list[j].getName();
+            Name2 = list[indexSmallest].getName();
+            // compare the director at the smallest index and the index before the smallest
+            compare = Name2.compare(Name1);
+            // swap the director names if the index before the smallest is actually the smallest
+            if (compare >= 1) {
+                indexSmallest = j;
+            }
+        }
+        swap(list[i], list[indexSmallest]);
+    }
+
+    int low = 0;
+    int high = list.size() - 1;
+    int mid;
+
+    while (high >= low) {
+        mid = (low + high) / 2;
+        // if the middle song title is lower than then the song title that is being searched for change the low and mid
+        if (list[mid].getName().compare(Name) < 0) {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        }
+            // if the middle song title is higher than then the song title that is being searched for change the high and mid
+        else if(list[mid].getName().compare(Name) > 0){
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+        else{
+            return mid;
+        }
+        return -1;
+    }
+}
 
 //getters
 int Inventory::getStock(){
@@ -160,10 +293,9 @@ string Inventory::getGenre(){
 //setters
 
 void setMovieID(int movieID){
-	this -> movieID = movieID;
+    this -> movieID = movieID;
 }
-voidsetStock(int stock){
-	this -> stock = stock;
+void setStock(int stock){
+    this -> stock = stock;
 }
-// display functions 
 

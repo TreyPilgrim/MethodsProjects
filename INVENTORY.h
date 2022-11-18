10899fb7d1433405ecc053608d8244471721a784
desreq <117791629+desreq@users.noreply.github.com>
2022-11-17 21:38:44 -0600

    Update INVENTORY.h

--- INVENTORY.h
+++ INVENTORY.h
@@ -1,21 +1,23 @@
 # include <string>
+# include <vector>
+using namespace std;
 class Inventory{
 	protected:
-	string Name;
-	string directorName;
-	string genre;
-	int price;
-	int stock;
-	int movieID;
+	string Name="";
+	string directorName="";
+	string genre="";
+	float price=0.0;
+	int stock=0;
+	int movieID=0;
 	public:
 	Inventory();
-	Inventory(string Name, string directorName, string genre, int price, int stock, int movieID)
+	Inventory(string Name, string directorName, string genre, float price, int stock, int movieID);
 	void readfile(vector<Inventory> &list);
-	void viewAllInventory();
-	void viewByDirector();
-	void viewByGenre();
-	void viewLowtoHigh();
-	void ViewHighttoLow();
+	void viewAllInventory(vector<Inventory> &list);
+	void viewByDirector(vector<Inventory> &list);
+	void viewByGenre(vector<Inventory> &list);
+	void viewLowtoHigh(vector<Inventory> &list);
+	void ViewHightoLow(vector<Inventory> &list);
 	int getStock();
 	int getPrice();
 	string getName();
@@ -24,10 +26,11 @@ class Inventory{
 	string getGenre();
 	void setMovieID(int movieID);
 	void setStock(int price);
-	bool searchByName(sting Name);
-	bool serchByDirector(string directorName);
-	bool searchByGenre(string genre);
+	bool searchByName(vector<Inventory>&list ,string Name);
+	bool searchByDirector(vector<Inventory>&list, string directorName);
+	bool searchByGenre(vector<Inventory>&list, string genre);
 	void processFile();
-}
+
+};
 	
 	

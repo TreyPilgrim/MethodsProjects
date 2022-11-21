274a331966059b3fc02dfd2e81d24549f3729c2d
desreq <117791629+desreq@users.noreply.github.com>
2022-11-21 11:28:24 -0600

    Update INVENTORY.h

--- INVENTORY.h
+++ INVENTORY.h
@@ -2,35 +2,36 @@
 # include <vector>
 using namespace std;
 class Inventory{
-	protected:
-	string Name="";
-	string directorName="";
-	string genre="";
-	float price=0.0;
-	int stock=0;
-	int movieID=0;
-	public:
-	Inventory();
-	Inventory(string Name, string directorName, string genre, float price, int stock, int movieID);
-	void readfile(vector<Inventory> &list);
-	void viewAllInventory(vector<Inventory> &list);
-	void viewByDirector(vector<Inventory> &list);
-	void viewByGenre(vector<Inventory> &list);
-	void viewLowtoHigh(vector<Inventory> &list);
-	void ViewHightoLow(vector<Inventory> &list);
-	int getStock();
-	int getPrice();
-	string getName();
-	int getMovieID();
-	string getDirectorName();
-	string getGenre();
-	void setMovieID(int movieID);
-	void setStock(int price);
-	bool searchByName(vector<Inventory>&list ,string Name);
-	bool searchByDirector(vector<Inventory>&list, string directorName);
-	bool searchByGenre(vector<Inventory>&list, string genre);
-	void processFile();
-
+protected:
+    string Name="";
+    string directorName="";
+    string genre="";
+    float price=0.0;
+    int stock=0;
+    string movieID="";
+public:
+    Inventory();
+    Inventory(string Name, string directorName, string genre, float price, int stock, string movieID);
+    void viewAllInventory(vector<Inventory> &list);
+    void viewByDirector(vector<Inventory> &list);
+    void viewByGenre(vector<Inventory> &list);
+    void viewLowtoHigh(vector<Inventory> &list);
+    void ViewHightoLow(vector<Inventory> &list);
+    int getStock();
+    int getPrice();
+    string getName();
+    string getMovieID();
+    string getDirectorName();
+    string getGenre();
+    void setMovieID(string movieID);
+    void setStock(int stock);
+    void setName(string Name);
+    void setPrice (int price);
+    void setDirectorName(string directorName);
+    void setGenre(string genre);
+    bool searchByName(vector<Inventory>&list ,string Name);
+    bool searchByDirector(vector<Inventory>&list, string directorName);
+    bool searchByGenre(vector<Inventory>&list, string genre);
 };
 	
 	

80acdd1248aaddfba4375a2e5d9fe2374e90478e
desreq <117791629+desreq@users.noreply.github.com>
2022-11-21 13:07:53 -0600

    Add files via upload

---
+++ main-commerce.cpp
@@ -0,0 +1,54 @@
+#include"INVENTORY.h"
+#include "shoppingcart.h"
+int main(){
+while (1) {
+        string option;
+        // displays menu
+        cout << "Menu: " << endl;
+        cout << "0. Exit" << endl;
+        cout << "1. Login" << endl;
+        cout << "2. Create Account" << endl;
+        cout << "3. Forgot Password" << endl
+        cout << "Enter choice: ";
+        cin >> option;
+        cout << endl;
+        // exit
+        if (option == "0") {
+            cout << "Thank You for Shopping with us" << endl;
+            break;
+        }
+            //  add menu option handling
+
+        else if (option == "1") {
+		string option2;
+		while(1){
+        // displays menu
+        cout << "Menu: " << endl;
+        cout << "1. View Cart" << endl;
+        cout << "2. View Inventory" << endl;
+        cout << "3. Checkout" << endl;
+        cout << "4. Edit Account" << endl
+		cout << "5. View Order History" << endl;
+		cout << "6. Logout" << endl;
+		cout << "7. Delete Account"<< endl;
+        cout << "Enter choice: ";
+        cin >> option2;
+        cout << endl;
+		}
+  
+
+        } 
+		else if (option == "2") {
+          
+
+        } 
+		else if (option == "3") {
+    
+		}
+        else {
+            // if they give an invalid option display error message and ask for another option
+            cout << "Invalid Choice! Choose again." << endl;
+            continue;
+        }
+        return 0;
+    }

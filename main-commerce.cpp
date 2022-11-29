ed73b0c110a892a04b8f91fe2418ce2d93285327
desreq <117791629+desreq@users.noreply.github.com>
2022-11-29 09:27:52 -0600

    Update main-commerce.cpp
    Fixed brackets problem

--- main-commerce.cpp
+++ main-commerce.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -57,200 +58,204 @@ int main() {
                     continue;
                 }
                 if(option2=="2"){
-                  cout<< "Display Options"<<endl;
-                  cout << "1. View All " << endl;
-                  cout << "2. View by Genre " << endl;
-                  cout << "3. View by Director" << endl;
-                  cout << "4. View Low to High " << endl;
-                  cout << "5. View High to Low " << endl;
-                  cout << "6.  Search By Name " << endl;
-                  cout << "7.  Search By Genre " << endl;
-                  cout << "8.  Search By Director " << endl;
+                    cout<< "Display Options"<<endl;
+                    cout << "1. View All " << endl;
+                    cout << "2. View by Genre " << endl;
+                    cout << "3. View by Director" << endl;
+                    cout << "4. View Low to High " << endl;
+                    cout << "5. View High to Low " << endl;
+                    cout << "6.  Search By Name " << endl;
+                    cout << "7.  Search By Genre " << endl;
+                    cout << "8.  Search By Director " << endl;
 
-                  if(option == "1") {
-                      string addOption;
-                      A.viewAllInventory(list);
-                      display(list);
-                      cout << "Do you want to add an item? Y or N" << endl;
-                      cin >> addOption;
-                      if (addOption == "Y" or addOption == "y") {
-                          int InventoryOption;
-                          B.addToCart(list, items);
-                      } else {
-                          break;
-                      }
-                      if (option == "2") {
-                          string addOption;
-                          A.viewByGenre(list);
-                          display(list);
-                          cout << "Do you want to add an item? Y or N" << endl;
-                          cin >> addOption;
-                          if (addOption == "Y" or addOption == "y") {
-                              int InventoryOption;
-                              B.addToCart(list, items);
-                          } else {
-                              break;
-                          }
-                      }
-                      if (option == "3") {
-                          string addOption;
-                          A.viewByDirector(list);
-                          display(list);
-                          cout << "Do you want to add an item? Y or N" << endl;
-                          cin >> addOption;
-                          if (addOption == "Y" or addOption == "y") {
-                              int InventoryOption;
-                              B.addToCart(list, items);
-                          } else {
-                              break;
-                          }
+                    if(option == "1") {
+                        string addOption;
+                        A.viewAllInventory(list);
+                        display(list);
+                        cout << "Do you want to add an item? Y or N" << endl;
+                        cin >> addOption;
+                        if (addOption == "Y" or addOption == "y") {
+                            int InventoryOption;
+                            B.addToCart(list, items);
+                        }
+                        else {
+                            break;
+                        }
 
-                      }
-                      if (option == "4") {
-                          string addOption;
-                          A.viewLowtoHigh(list);
-                          display(list);
-                          cout << "Do you want to add an item? Y or N" << endl;
-                          cin >> addOption;
-                          if (addOption == "Y" or addOption == "y") {
-                              int InventoryOption;
-                              B.addToCart(list, items);
-                          } else {
-                              break;
-                          }
-                      }
-                      if (option == "5") {
-                          string addOption;
-                          A.viewHightoLow(list);
-                          display(list);
-                          cout << "Do you want to add an item? Y or N" << endl;
-                          cin >> addOption;
-                          if (addOption == "Y" or addOption == "y") {
-                              int InventoryOption;
-                              B.
-                                  addToCart(list, items);
-                          } else {
-                              break;
-                          }
+                    }
+                    if (option == "2") {
+                        string addOption;
+                        A.viewByGenre(list);
+                        display(list);
+                        cout << "Do you want to add an item? Y or N" << endl;
+                        cin >> addOption;
+                        if (addOption == "Y" or addOption == "y") {
+                            int InventoryOption;
+                            B.addToCart(list, items);
+                        } else {
+                            break;
+                        }
+                    }
+                    if (option == "3") {
+                        string addOption;
+                        A.viewByDirector(list);
+                        display(list);
+                        cout << "Do you want to add an item? Y or N" << endl;
+                        cin >> addOption;
+                        if (addOption == "Y" or addOption == "y") {
+                            int InventoryOption;
+                            B.addToCart(list, items);
+                        } else {
+                            break;
+                        }
 
-                      }
-                      if (option == "6"){
-                      cout <<"not finished yet;"  << endl;
-                      }
-                      if (option == "7"){
-                          cout <<"not finished yet;"  << endl;
-                      }
-                      if (option == "8"){
-                          cout <<"not finished yet;"  << endl;
-                      }
-                  }
+                    }
+                    if (option == "4") {
+                        string addOption;
+                        A.viewLowtoHigh(list);
+                        display(list);
+                        cout << "Do you want to add an item? Y or N" << endl;
+                        cin >> addOption;
+                        if (addOption == "Y" or addOption == "y") {
+                            int InventoryOption;
+                            B.addToCart(list, items);
+                        } else {
+                            break;
+                        }
+                    }
+                    if (option == "5") {
+                        string addOption;
+                        A.viewHightoLow(list);
+                        display(list);
+                        cout << "Do you want to add an item? Y or N" << endl;
+                        cin >> addOption;
+                        if (addOption == "Y" or addOption == "y") {
+                            int InventoryOption;
+                            B.
+                                    addToCart(list, items);
+                        } else {
+                            break;
+                        }
 
-            else if (option == "2") {
-                cout << "not completed";
+                    }
+                    if (option == "6"){
+                        cout <<"not finished yet;"  << endl;
+                    }
+                    if (option == "7"){
+                        cout <<"not finished yet;"  << endl;
+                    }
+                    if (option == "8"){
+                        cout <<"not finished yet;"  << endl;
+                    }
+                }
+
+                else if (option == "2") {
+                    cout << "not completed";
+
+                }
+                else if (option == "3") {
+                    cout << "not completed";
+
+                }
+                else {
+                    // if they give an invalid option display error message and ask for another option
+                    cout << "Invalid Choice! Choose again." << endl;
+                    continue;
+                }
+                return 0;
+            }
+        }
+    }}
+        int convertStringtoInt(string str){
+            stringstream ss;
+            ss << str;
+            int num;
+            ss>> num;
+            return num;
+        }
+        float convertStringtoFloat(string str){
+            stringstream ss;
+            ss << str;
+            float num;
+            ss>> num;
+            return num;
+        }
+
+
+        void readfile(vector<Inventory>&list)
+        {
+            ifstream infile;
+            string line;
+
+
+            infile.open("INVENTORY.txt");
+
+            if(infile.is_open())
+            {
+                cout << "Successful inventory opening." << endl;
+            }
+
+            else
+            {
+                cout << "Couldn't locate file. Program closing." << endl;
+                exit(EXIT_FAILURE);
+            }
+
+
+            while(getline(infile, line))
+            {
+
+                string Name, directorName, genre,price,stock, movieID;
+                float price1;
+                float stock1;
+                Name = line;
+
+                getline(infile,line);
+                directorName = line;
+
+                getline(infile,line);
+                genre = line;
+
+                getline(infile,line);
+                price=line;
+
+                getline(infile,line);
+                stock =line;
+
+                getline(infile,line);
+                movieID =line;
+
+                getline(infile, line);
+
+                price1 = convertStringtoFloat(price);
+                stock1 = convertStringtoInt(stock);
+
+                Inventory tmp;
+                tmp.setName(Name);
+
+                tmp.setDirectorName(directorName);
+                tmp.setGenre(genre);
+                tmp.setPrice(price1);
+                tmp.setStock(stock1);
+                tmp.setMovieID(movieID);
+
+                list.push_back(tmp);
+
 
             }
-            else if (option == "3") {
-                cout << "not completed";
 
+            infile.close();
+            list.pop_back();
+
+        }
+        void display(vector<Inventory>list){
+            for (int i = 0; i < list.size(); i++) {
+                Inventory currItem;
+                currItem = list.at(i);
+                cout << i + 1 << ". " << currItem.getName() << endl;
+                cout << "Director:" << currItem.getDirectorName() << endl;
+                cout << "Genre:" << currItem.getGenre() << endl;
+                cout << "Price:" << currItem.getPrice() << endl;
             }
-            else {
-                // if they give an invalid option display error message and ask for another option
-                cout << "Invalid Choice! Choose again." << endl;
-                continue;
-            }
-            return 0;
-        };
-    }
-int convertStringtoInt(string str){
-    stringstream ss;
-    ss << str;
-    int num;
-    ss>> num;
-    return num;
-}
-float convertStringtoFloat(string str){
-    stringstream ss;
-    ss << str;
-    float num;
-    ss>> num;
-    return num;
-}
-
-
-  void readfile(vector<Inventory>&list)
-{
-    ifstream infile;
-    string line;
-
-
-    infile.open("INVENTORY.txt");
-
-    if(infile.is_open())
-    {
-        cout << "Successful inventory opening." << endl;
-    }
-
-    else
-    {
-        cout << "Couldn't locate file. Program closing." << endl;
-        exit(EXIT_FAILURE);
-    }
-
-
-    while(getline(infile, line))
-    {
-
-        string Name, directorName, genre,price,stock, movieID;
-        float price1;
-        float stock1;
-        Name = line;
-
-        getline(infile,line);
-        directorName = line;
-      
-        getline(infile,line);
-        genre = line;
-       
-        getline(infile,line);
-        price=line;
-        
-        getline(infile,line);
-        stock =line;
-       
-        getline(infile,line);
-        movieID =line;
-       
-        getline(infile, line);
-
-        price1 = convertStringtoFloat(price);
-        stock1 = convertStringtoInt(stock);
-
-        Inventory tmp;
-        tmp.setName(Name);
-        
-        tmp.setDirectorName(directorName);
-        tmp.setGenre(genre);
-        tmp.setPrice(price1);
-        tmp.setStock(stock1);
-        tmp.setMovieID(movieID);
-        
-        list.push_back(tmp);
-        
-
-    }
-
-    infile.close();
-    list.pop_back();
-  
-}
-void display(vector<Inventory>list){
-    for(int i=0; i< list.size(); i++){
-        for (int i = 0; i < list.size(); i++) {
-        Inventory currItem;
-        currItem = list.at(i);
-        cout << i + 1 << ". " << currItem.getName() << endl;
-        cout << "Director:" << currItem.getDirectorName() << endl;
-        cout << "Genre:" << currItem.getGenre() << endl;
-        cout << "Price:" << currItem.getPrice() << endl;
-    }
+        }
             

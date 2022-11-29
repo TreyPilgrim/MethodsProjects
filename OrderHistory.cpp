dc4283a65fef646f262eee33a944e4e5f74e1b6f
Kenja21 <99065019+Kenja21@users.noreply.github.com>
2022-11-29 11:08:25 -0600

    Create OrderHistory.cpp

---
+++ OrderHistory.cpp
@@ -0,0 +1,47 @@
+#include "OrderHistory.h"
+
+
+OrderHistory::OrderHistory() {
+
+	std::string UserName = " ";
+
+}
+
+std::string OrderHistory::setUserName(std::string username) {
+
+	this->UserName = UserName;
+}
+
+std::string OrderHistory::setOrderContents(std::string item) {
+	
+	item = curre
+}
+
+
+void AddOrdHistory(string& item, vector<ShoppingCart>& cart, std::string OrderFile) {
+
+	ShoppingCart currentitem;
+
+	for (i = 0; i < cart.size; i++) {
+
+		current item = cart.at(i);
+
+		if (UserName == UserName)
+		{
+			setOrderContents(currentitem.GetName())
+			setUserName(currentitem.GetUserName())
+			fstream file;
+
+			file.open(OrderFile, ios::app);
+			if (file.is_open()) {
+				file << 
+			}
+		}
+
+
+
+	}
+
+}
+void ViewOrdHistory();
+void DeleteOrdHistory();

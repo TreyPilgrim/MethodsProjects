f6029dcf4e1b229e25cf290d3048d87eeefe746d
Trey Fullwood <treyfullwood3@gmail.com>
2022-11-30 12:51:41 -0600

    Official Payment.h

--- Payment.cpp
+++ Payment.cpp
@@ -1,5 +1,33 @@
-//
-// Created by treyf on 11/26/2022.
-//
+#pragma once
+#include <string>
 
-#include "Payment.h"
+using std::string;
+class Payment
+{
+private:
+
+    int cardNum {0}; // Last 4 digits of card
+    int expDate {0000};
+    int securityCode {};
+    string cardName {"Default"};
+
+public:
+
+    // getters
+    int getCardNum();
+    int getExpDate() const;
+    int getSecurCode() const;
+    string getName();
+
+    //setters
+    void setCardNum(int num);
+    void setExpDate(int date);
+    void setSecurCode(int code);
+    void setCardName(string name);
+
+    // Checkers
+    static bool checkCardNum(int num);
+    static bool checkExpDate(int date);
+    static bool checkSecurCode(int code);
+
+};

8cb25b8aca5788228395066ac280b8ec7d1e7149
Trey Fullwood <treyfullwood3@gmail.com>
2022-11-30 12:52:24 -0600

    Official Payment.h

--- Payment.h
+++ Payment.h
@@ -4,9 +4,30 @@
 using std::string;
 class Payment
 {
-    int cardNum {};
+private:
+
+    int cardNum {0}; // Last 4 digits of card
     int expDate {0000};
     int securityCode {};
     string cardName {"Default"};
+
+public:
+
+    // getters
+    int getCardNum();
+    int getExpDate() const;
+    int getSecurCode() const;
+    string getName();
+
+    //setters
+    void setCardNum(int num);
+    void setExpDate(int date);
+    void setSecurCode(int code);
+    void setCardName(string name);
+
+    // Checkers
+    static bool checkCardNum(int num);
+    static bool checkExpDate(int date);
+    static bool checkSecurCode(int code);
 
 };

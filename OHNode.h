0539c4e7550c911b20b936e45bcbc4ca0af77721
desreq <117791629+desreq@users.noreply.github.com>
2022-11-29 19:51:12 -0600

    Update OHNode.h

--- OHNode.h
+++ OHNode.h
@@ -1,14 +1,17 @@
 #pragma once
+# include<string>
+using namespace std;
 class node
 {
 public:
 	int data;
 	node *next;
 	node *prev;
+    string UserName ="";
+    string OrderContents ="";
 
 	node(): data(0), next(nullptr), prev(nullptr){}
-	node(int data): data(data), next(nullptr),prev(nullptr){}
-	node(int data, node* next, node* prev) : data(data),
-		next(next), prev(prev) {};
+	node(string UserName, string OrderContents): UserName(UserName),OrderContents(OrderContents), next(nullptr),prev(nullptr){}
+	node(string UserName, string OrderContents, node* next, node* prev) : UserName(UserName),OrderContents(OrderContents), next(next), prev(prev) {};
 
 };

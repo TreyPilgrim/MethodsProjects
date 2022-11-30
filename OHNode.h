65b458a52b90d3601aa54a5a2097edb0821deccc
desreq <117791629+desreq@users.noreply.github.com>
2022-11-29 20:22:33 -0600

    Update OHNode.h

--- OHNode.h
+++ OHNode.h
@@ -10,7 +10,7 @@ public:
     string UserName ="";
     string OrderContents ="";
 
-	node(): data(0), next(nullptr), prev(nullptr){}
+	node(): UserName(),OrderContents(), next(nullptr), prev(nullptr){}
 	node(string UserName, string OrderContents): UserName(UserName),OrderContents(OrderContents), next(nullptr),prev(nullptr){}
 	node(string UserName, string OrderContents, node* next, node* prev) : UserName(UserName),OrderContents(OrderContents), next(next), prev(prev) {};
 

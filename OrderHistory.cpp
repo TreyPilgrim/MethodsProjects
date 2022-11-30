394fe5d67023b2319b04357431b5443d449c5dfa
desreq <117791629+desreq@users.noreply.github.com>
2022-11-29 19:14:02 -0600

    Add files via upload

--- OrderHistory.cpp
+++ OrderHistory.cpp
@@ -1,11 +1,8 @@
 #include "OrderHistory.h"
+#include <string>
+#include "ShoppingCart.h"
 
 
-OrderHistory::OrderHistory() {
-
-	std::string UserName = " ";
-
-}
 
 std::string OrderHistory::setUserName(std::string username) {
 
@@ -44,3 +41,76 @@ void AddOrdHistory(string& item, vector<ShoppingCart>& cart, std::string OrderFi
 }
 void ViewOrdHistory();
 void DeleteOrdHistory();
+
+void OrderHistory::append(int data) {
+
+	node* tmp = new node(data);
+	tmp->prev = tail;
+
+	if (head == nullptr)
+	{
+		head = tmp;
+	}
+	else
+	{
+		tail->next = tmp;
+	}
+	tail = tmp;
+}
+bool OrderHistory::remove(int data) {
+	node* tmp = head;
+	while (tmp != nullptr)
+	{
+		if (tmp->data == data)
+		{
+			if (tmp->prev != nullptr)
+			{
+				tmp->prev->next = tmp->next;
+			}
+			if (tmp->next != nullptr)
+			{
+				tmp->next->prev = tmp->prev;
+
+			}
+			if (tmp == head)
+			{
+				head = tmp->next;
+
+			}
+			if (tmp == tail)
+			{
+				tail = tmp->prev;
+			}
+
+			delete tmp;
+			return true;
+		}
+		tmp = tmp->next;
+	}
+	return false;
+
+}
+void OrderHistory::display(int data, std::ostream &os) {
+	node* tmp = head;
+	
+	while (tmp != nullptr)
+	{
+		os << tmp->data << std::endl;
+		tmp = tmp->next;
+	}
+}
+void OrderHistory::prepend(int data) {
+	node* tmp = new node(data);
+
+	tmp->next = head;
+
+	if (head == nullptr)
+	{
+		tail = tmp;
+	}
+	else
+	{
+		head->prev = tmp;
+	}
+	head = tmp;
+}
--- OrderHistory.h
+++ OrderHistory.h
@@ -1,24 +1,33 @@
-#pragma once
-#include<string>
-#include<iostream>
 
-using namespace std;
+#include "ShoppingCart.h"
+# include "OHNode.h"
+
+
 class OrderHistory
 {
 private:
 	string item;
+	string UserName;
 	string OrderContents;
+	node* head;
+	node* tail;
 
 
 public:
+	OrderHistory(): head(nullptr), tail(nullptr) {}
+	~OrderHistory();
+	void append(string UserName, string OrderContents);
+	bool remove(string UserName, string OrderContents);
+	void display(string UserName, string OrderContents, std::ostream &os);
+	void prepend(string UserName, string OrderContents);
 	void AddOrdHistory(string &item, vector<ShoppingCart> &cart);
 	void ViewOrdHistory();
 	void DeleteOrdHistory();
 
 	void setOrdContents();
 	std::string SetUserName(std:: string username);
-	
-	
+
+
 	void getCartID(int);
 
 };
